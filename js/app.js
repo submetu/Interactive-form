7742565993fa0ae2dd15335f7afb7a010428b41d
subhan <sub.metu@gmail.com>
2016-09-09 13:46:50 +0300

    update FINAL

--- js/app.js
+++ js/app.js
@@ -6,22 +6,20 @@ var checkedNode=false;
 var checkedBuilTools=false;
 var checkedNpm=false;
 var Total=0;
-var valid;
-var $TotalPara=$('<p>$</p>');
 //Creat the JAVA PUN select element, clone the options,  and append it to the page
 var $PunsSelect=$('<select id="puns"></select>');
-var $PunsOpt1=$('#color').children().eq(0).clone().appendTo($PunsSelect);
-var $PunsOpt2=$('#color').children().eq(1).clone().appendTo($PunsSelect);
-var $PunsOpt3=$('#color').children().eq(2).clone().appendTo($PunsSelect);
+$('#color').children().eq(0).clone().appendTo($PunsSelect);
+$('#color').children().eq(1).clone().appendTo($PunsSelect);
+$('#color').children().eq(2).clone().appendTo($PunsSelect);
 $PunsSelect.addClass('selectpicker');
 $('#colors-js-puns').append($PunsSelect);
 $PunsSelect.hide();
 
 //Create the I HEART JAVA select element, clone the options, and append it to the page
 var $LoveSelect=$('<select id="love"></select>');
-var $LoveOpt1=$('#color').children().eq(3).clone().appendTo($LoveSelect);
-var $LoveOpt2=$('#color').children().eq(4).clone().appendTo($LoveSelect);
-var $LoveOpt3=$('#color').children().eq(5).clone().appendTo($LoveSelect);
+$('#color').children().eq(3).clone().appendTo($LoveSelect);
+$('#color').children().eq(4).clone().appendTo($LoveSelect);
+$('#color').children().eq(5).clone().appendTo($LoveSelect);
 $LoveSelect.addClass('selectpicker');
 $('#colors-js-puns').append($LoveSelect);
 $LoveSelect.hide();
@@ -43,18 +41,19 @@ function HideErrors(){
     $paymentError.appendTo($('#payment').prev()).hide();
     $creditCardError.prependTo($('#credit-card')).hide();
 }
-
+//Checks if the email is valid or not
 function isEmail(email) {
   var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   return regex.test(email);
 }
+//function that hides the previous total price and updates it in the dom with the updated Total value
 function showHideTotal(){
     $('fieldset.activities p').remove();
     var $TotalPara=$('<p>$</p>');
     $TotalPara.append(Total);
     $('fieldset.activities').append($TotalPara);
-    console.log(Total);
 }
+//function that takes in a string and shows/hides appropriate information depending on the argument given to it
 function showHidePayment(string){
     switch(string){
         case "select":
@@ -80,45 +79,40 @@ function showHidePayment(string){
     }
 }
 //function ran when the form submit button is pressed
-function validateMyForm(){
+function validateMyForm(){ //THIS FUNCTION IS RUN FROM THE HTML (SEE LINE 21 IN INDEX.HTML)
     //hide all errors
     HideErrors();
     if($('input#name').val()===''){ //if the name text field is empty
         event.preventDefault();//prevent the browser from submitting the form
-        $('html, body').animate({ scrollTop: 0 }, 'fast'); //animate to the top of the page
+        $('html, body').animate({ scrollTop: 0 }, 'fast'); //animate scroll to the top of the page
         $nameError.show();
-        console.log("name is empty");
     }
     if(!isEmail($('#mail').val())){ //if the email address entered is not true
         event.preventDefault();
         $('html, body').animate({ scrollTop: 0 }, 'fast');
         $emailError.show();
-        console.log("email address is false");
     }
     if($('.activities input:checked').length <=0){ //if there are no checked activities
         event.preventDefault();
         $('html, body').animate({ scrollTop: 0 }, 'fast');
         $checkboxError.show();
-        console.log('no acitivities selected');
     }
-    if($('#payment').val()==="select_method"){
+    if($('#payment').val()==="select_method"){ //if the payment method is not selected
         event.preventDefault();
         $('html, body').animate({ scrollTop: 0 }, 'fast');
         $paymentError.show();  
-        console.log('no payment method selected');
     }
-    var result= $('#cc-num').validateCreditCard();
-    if(!result.length_valid || !result.luhn_valid){
+    var result= $('#cc-num').validateCreditCard(); //store the validateCreditCard return object in result
+    if(!result.length_valid || !result.luhn_valid){ //if either the length of creditcard number is invalid or the luhn check fails
         event.preventDefault();
         $creditCardError.show();
-        console.log('wrong credit card');
     }
-    if($('#cvv').val().length!==3){
+    if($('#cvv').val().length!==3){ //if the cvv value is not equal to 3
         event.preventDefault();
         $creditCardError.show();
         
     }
-    if($('#zip').val().length<=0){
+    if($('#zip').val().length<=0){ //if the zip value is empty
         event.preventDefault();
         $creditCardError.show();
         
@@ -129,12 +123,12 @@ function validateMyForm(){
 
 //EVENT LISTENERS
 
-//Allow only numbers to be entered
+//Allow only numbers to be entered in the zip field
 $('#zip').on('keypress', function(evt) {
     var charCode = (evt.which) ? evt.which : event.keyCode;
     return !(charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57));
 });
-//Allow only numbers to be entered
+//Allow only numbers to be entered in the cvv field
 $('#cvv').on('keypress', function(evt) {
     var charCode = (evt.which) ? evt.which : event.keyCode;
     return !(charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57));
@@ -155,16 +149,19 @@ $('#title').on("change",function(){ //when there is change in the select element
 $("#design").on("change",function(){ //when there is change on this select element
     
     if($(this).val()==="js puns"){ //if the value of the option selected is 'js puns'
+        //SHOW THE JS PUNS SELECT MENU AND HIDE THE I LOVE JS SELECT MENU
         $("select#puns").parent().show();
         $('#colors-js-puns').show();
         $("select#love").parent().hide();//show the $PunsSelect select element
     }
     if($(this).val()==="heart js"){ //if the value of the option selected is 'heart js'
+        //SHOW THE I LOVE JS SELECT MENU AND HIDE THE JS PUNS SELECT MENU
         $("select#love").parent().show();
         $('#colors-js-puns').show();
         $("select#puns").parent().hide();//show the $LoveSelect select element
     }
     if ($(this).val()=="Select Theme"){ //if the value of the option selected is 'Select Theme'
+        //HIDE ALL THE SELECT MENUS
         $('#colors-js-puns').hide();
     }
 });
@@ -179,7 +176,7 @@ $('.activities input[name="all"]').on("change",function(){ //if there is a chang
             showHideTotal();//run the showHideTotal function
             checkedAll=true;//set this boolean to true
     } 
-    if($('input[name="all"]:checkbox'). prop("checked") != true){//if the checkbox is unchecked
+    if($('input[name="all"]:checkbox'). prop("checked") !== true){//if the checkbox is unchecked
         if(checkedAll){ //check if the checkbox went from a checked to an unchecked state
             Total-=200; //decrement by 200
             showHideTotal();//run the showHideTotal function
@@ -198,7 +195,7 @@ $('.activities input[name="js-frameworks"]').on("change",function(){//if there i
         showHideTotal();//run the showHideTotal function
         checkedFram=true;//set this boolean to True
     }
-    if($('input[name="js-frameworks"]:checkbox'). prop("checked") != true){ //if the checkbox is unchecked
+    if($('input[name="js-frameworks"]:checkbox'). prop("checked") !== true){ //if the checkbox is unchecked
         if(checkedFram){    //check if the checkbox went from a checked to an unchecked state
             $('input[name="express"]').prop("disabled",false);  //remove the disabled property on input[name="express"] 
             //remove the class disabled from the parent of input[name="express"]
@@ -221,7 +218,7 @@ $('.activities input[name="js-libs"]').on("change",function(){
         showHideTotal();
         checkedLib=true;
     }
-    if($('input[name="js-libs"]:checkbox'). prop("checked") != true){
+    if($('input[name="js-libs"]:checkbox'). prop("checked") !== true){
         if(checkedLib){
             $('input[name="node"]').prop("disabled",false);
             $('input[name="node"]').parent().removeClass("disabled");
@@ -241,7 +238,7 @@ $('.activities input[name="express"]').on("change",function(){
         showHideTotal();
         checkedExp=true;
     } 
-    if($('input[name="express"]:checkbox'). prop("checked") != true){
+    if($('input[name="express"]:checkbox'). prop("checked") !== true){
         if(checkedExp){
             $('input[name="js-frameworks"]').prop("disabled",false);
             $('input[name="js-frameworks"]').parent().removeClass("disabled");
@@ -262,7 +259,7 @@ $('.activities input[name="node"]').on("change",function(){
         showHideTotal();
         checkedNode=true;
     } 
-    if($('input[name="node"]'). prop("checked") != true){
+    if($('input[name="node"]'). prop("checked") !== true){
         if(checkedNode){
             $('input[name="js-libs"]').prop("disabled",false);
             $('input[name="js-libs"]').parent().removeClass("disabled");
@@ -281,7 +278,7 @@ $('.activities input[name="build-tools"]').on("change",function(){
             showHideTotal();
             checkedBuilTools=true;
     }
-    if($('input[name="build-tools"]:checkbox'). prop("checked") != true){
+    if($('input[name="build-tools"]:checkbox'). prop("checked") !== true){
         if(checkedBuilTools){
             Total-=100;
             showHideTotal();
@@ -300,7 +297,7 @@ $('.activities input[name="npm"]').on("change",function(){
         checkedNpm=true;
         console.log("npm check");
     }
-    if($('input[name="npm"]:checkbox'). prop("checked") != true){
+    if($('input[name="npm"]:checkbox'). prop("checked") !== true){
         if(checkedNpm){
             Total-=100;
             showHideTotal();
